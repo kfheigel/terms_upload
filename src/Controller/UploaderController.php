f3b847703521028aacd0d3893b3cc13308a52bc2
Krzysztof Heigel <krzysztof.heigel@home.net.pl>
2020-03-28 11:51:31 +0100

    Added first page controller

---
+++ src/Controller/UploaderController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+
+class UploaderController{
+
+    public function upload(){
+        
+        return new Response('Works!');
+
+    }
+}
