a9e4abccd832b52c9a0ec05ce487a768224ec923
Krzysztof Heigel <krzysztof.heigel@home.net.pl>
2020-04-01 12:03:05 +0200

    Added profiler and debug tools, css file

--- src/Controller/UploaderController.php
+++ src/Controller/UploaderController.php
@@ -14,15 +14,17 @@ class UploaderController extends AbstractController {
     public function Upload($service){
 
         $services = array('home', 'az', 'ionosbg', 'ionoshu', 'ionosro');
+
         if(in_array($service, $services)){
 
             return $this->render('uploader/upload.html.twig',[
-                'service' => $service
+                'service' => $service,
+                'services' => $services
             ]);
 
         }else {
             return $this->render('uploader/upload.html.twig',[
-                'service' => "Service: $service is not available",
+                'service' => "Service '$service' is not available",
                     ]);
 
 
