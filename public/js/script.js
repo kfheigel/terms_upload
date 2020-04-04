98e8794636da15e4817cda503a40e339e1f2c8eb
Krzysztof Heigel <krzysztof.heigel@home.net.pl>
2020-04-04 17:10:14 +0200

    Refactored form

--- public/js/script.js
+++ public/js/script.js
@@ -1,14 +1,20 @@
 function showname () {
     var fileName = document.getElementById('terms'),
         label = document.getElementById('input-label'),
+        submit = document.getElementById('submit-button'),
         labelVal = label.innerText;
 
     fileName = fileName.files.item(0).name;
     if( fileName ){
         label.innerHTML = fileName;
-        label.classList.add('btn-success');
+        label.classList.add('input-upload-blue');
         label.classList.remove('btn-primary');
-    }else {
-        label.innerHTML = labelVal;
+
+        submit.removeAttribute("disabled");
+        submit.classList.add('btn-success');
+        submit.classList.remove('btn-secondary', 'upload-text');
     }
 };
+
+document.querySelector("#date").valueAsDate = new Date();
+
