73c3e243ace4e0ff092cff49300f4d716afcaf9a
Krzysztof Heigel <krzysztof.heigel@home.net.pl>
2020-04-04 12:41:43 +0200

    Created more friendly form

--- src/Controller/UploaderController.php
+++ src/Controller/UploaderController.php
@@ -3,31 +3,22 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class UploaderController extends AbstractController {
 
     /**
-     * @Route("/uploader/{service}")
+     * @Route("/", name="uploader_service")
      */
-    public function Upload($service){
+    public function site(){
+        return $this->render('uploader/upload.html.twig');
+    }
 
-        $services = array('home', 'az', 'ionosbg', 'ionoshu', 'ionosro');
-
-        if(in_array($service, $services)){
-
-            return $this->render('uploader/upload.html.twig',[
-                'service' => $service,
-                'services' => $services
-            ]);
-
-        }else {
-            return $this->render('uploader/upload.html.twig',[
-                'service' => "Service '$service' is not available",
-                    ]);
-
-
-        }
+    /**
+     * @Route("/", name="upload_terms")
+     */
+    public function termUpload(Request $request){
+        dd($request->files->get('terms'));
     }
 }
