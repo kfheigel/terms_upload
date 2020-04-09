dc5491a5da1bc8851f245b939ec09b5efa558116
Krzysztof Heigel <krzysztof.heigel@home.net.pl>
2020-04-09 19:57:07 +0200

    Deleted unnecesary class FormUploader

--- src/Controller/UploaderController.php
+++ src/Controller/UploaderController.php
@@ -2,7 +2,6 @@
 
 namespace App\Controller;
 
-use App\Entity\FormUploader;
 use App\Form\FormUploaderType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
