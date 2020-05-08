3c7cbd7b6e268ca4f8e5b96e59dd61aef5561e10
Krzysztof Heigel <krzysztof.heigel@home.net.pl>
2020-05-08 11:43:40 +0200

    Finished file name validation

--- src/Form/FormUploaderType.php
+++ src/Form/FormUploaderType.php
@@ -6,10 +6,13 @@ use App\Entity\FormUploader;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
-use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Callback;
+use Symfony\Component\Validator\Constraints\File;
+use Symfony\Component\Validator\Context\ExecutionContextInterface;
 
 class FormUploaderType extends AbstractType
 {
@@ -27,8 +30,23 @@ class FormUploaderType extends AbstractType
                     ],
                 'mapped'        => false,
             ])
-            ->add('date', DateType::class)
-            ->add('terms', FileType::class)
+            ->add('terms', FileType::class, [
+                'constraints'   => [
+                    new Callback(function (UploadedFile $object, ExecutionContextInterface $context, $payload) {
+                        $pattern = '/^([a-z]+-)+20\d{6}\.pdf/';
+                        if (!preg_match($pattern, $object->getClientOriginalName())) {
+                            $context->buildViolation('Zmień nazwę pliku zgodnie z wytycznymi poniżej:')
+                                ->addViolation();
+                        }
+                    }),
+                    new File([
+                        'mimeTypes' => [
+                            "application/pdf",
+                            "application/x-pdf",
+                        ],
+                        'mimeTypesMessage' => 'Wrzuć plik PDF z poprawnym rozszerzeniem',
+                    ]),
+                ]])
             ->add('button', SubmitType::class);
     }
 
